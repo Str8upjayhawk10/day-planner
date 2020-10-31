266b76e61d068858108c2e1f666418cb7d8f56b6
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-10-30 19:45:33 -0700

    imported font awesome

--- apps.js
+++ apps.js
@@ -1,9 +1,15 @@
 // jQuery fuction ready to call
 $(document).ready(function(){
     console.log(parseInt(moment().format('H')))
+
     // Created variable global space/saved using const display military hour without minutes
     const currentTime = moment().format('Do YYYY MMMM');
     
+    // The submit event is sent to the element "form" when the user is attempting to submit a form
+    $('form').on('submit', function (e) {  
+        e.preventDefault()
+        console.log(e.target)
+    })
     
 
 
