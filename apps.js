527f4e48ba265fdee0b973ecf98409d220e45dc2
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-10-30 21:55:17 -0700

    line:16-21 array for loop process of tracking time

--- apps.js
+++ apps.js
@@ -5,7 +5,20 @@ $(document).ready(function(){
     console.log(parseInt(moment().format('H')))
 
     // Created variable global space/saved using const display military hour without minutes
-    const currentTime = moment().format('Do YYYY MMMM');
+    const currentTime = moment().format('MMMM, Do, YYYY');
+    // call currentTime to be displayed
+    $('#currentTime').text(currentTime);
+    // attr set to return span attribute used style to target width 
+    $('span').attr('style', 'width: 65px');
+    // target button color blue
+    $('button').attr('style', 'background-color: blue',);
+
+    // create an array to keep track of time
+    const times = [9,10,11,12,13,14,15,16,17]
+    // create a loop using forEach method concise and less error prone
+    times.forEach(time => {
+        console.log(times)
+    }) 
     
     // The submit event is sent to the element "form" when the user is attempting to submit a form
     $('form').on('submit', function (e) {  
