2c347f5bdb96fbde338dd444b48eef8f7e939967
Nathaniel Meadows <str8upjayhawk10@gmail.com>
2020-10-30 20:38:01 -0700

    line:14-22 targeted element return a value det local storage to display time & text

--- apps.js
+++ apps.js
@@ -1,5 +1,7 @@
 // jQuery fuction ready to call
 $(document).ready(function(){
+
+    //  parses a string and returns an integer # for 'H'
     console.log(parseInt(moment().format('H')))
 
     // Created variable global space/saved using const display military hour without minutes
@@ -8,7 +10,15 @@ $(document).ready(function(){
     // The submit event is sent to the element "form" when the user is attempting to submit a form
     $('form').on('submit', function (e) {  
         e.preventDefault()
-        console.log(e.target)
+
+        // target specified element "input" & returns the value of a specified attribute id "9am-17pm" on the element
+        const time = e.target.querySelector("input").getAttribute("id")
+
+        // used to get the values of form elements
+        const text = e.target.querySelector("input").value
+
+        // setItem localStorage stored within dev tools application time&text
+        window.localStorage.setItem(time, text)
     })
     
 
